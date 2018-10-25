ecdee88a633748af8860c115e555a07c91025c58
Mukesh-BR <32503710+Mukesh-BR@users.noreply.github.com>
2018-10-26 02:22:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from flask import Flask
-import numpy
+import numpy as np
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  #a = np.array([1,2,3])
-  return "Hello World"
+  a = np.array([1,2,3])
+  return str(a[0])
 
 if __name__ == '__main__':
   app.run()
