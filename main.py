24f4cc7f283a2aad20e4dd0e02d499e5a3489d02
Mukesh-BR <32503710+Mukesh-BR@users.noreply.github.com>
2018-10-26 14:06:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
-from flask import Flask
+from flask import Flask,request
 import numpy as np
-import sklearn
-app = Flask(__name__)
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.externals import joblib
+app=Flask(__name__)
 
 @app.route('/')
-def hello_world():
-  a = np.array([1,2,3])
-  return str(a[0])
 
-if __name__ == '__main__':
-  app.run()
+def main():
+	# a = request.headers.get('A')
+	# b = request.headers.get('B')
+	# c = request.headers.get('C')
+	# d = request.headers.get('D')
+
+	a=33
+	b=77
+	c=983
+	d=70
+	x_test = np.array([a,b,c,d]).reshape(1,4)
+
+	clf_ne = joblib.load('model_ne.pkl') 
+	clf_se = joblib.load('model_se.pkl') 
+	clf_sw = joblib.load('model_sw.pkl') 
+	clf_nw = joblib.load('model_nw.pkl') 
+
+	ypred_ne=str(abs(clf_ne.predict(x_test)[0]))	
+	ypred_se=str(abs(clf_se.predict(x_test)[0]))
+	ypred_sw=str(abs(clf_sw.predict(x_test)[0]))
+	ypred_nw=str(abs(clf_nw.predict(x_test)[0]))
+	
+	print("Hello World")
+	print(request.headers)
+	a=request.headers.get('A')
+	print(type(a))
+	return ypred_ne+" "+ypred_se+" "+ypred_sw+" "+ypred_nw 
+
+if __name__=='__main__':
+	app.run(host='192.168.43.48',port=8000)
+
+
+
+
 
 
