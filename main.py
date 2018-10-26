ad9ceed2030e347345c9d14c299c0c0d9a2129ea
Mukesh-BR <32503710+Mukesh-BR@users.noreply.github.com>
2018-10-26 18:05:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+  model = pickle.load(open("model.pkl", 'rb'))
   return 'Hey its Python Flask application!'
 
 if __name__ == '__main__':
   app.run()
-
-
-
-
 
 
 
